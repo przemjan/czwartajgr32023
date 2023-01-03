1ea5a5830daa79bbbe461c319cc46bc346ac8f01
przemjan <przemjanisz2@gmail.com>
2023-01-03 08:03:27 +0100

    Initial commit

---
+++ src/podstawy/Zmienne.java
@@ -0,0 +1,52 @@
+package podstawy;
+
+public class Zmienne {
+    public static void main(String[] args) {
+
+        int liczba1;
+        liczba1 = 123;
+
+        double liczba2 = 23.9;
+
+        System.out.println(liczba1);
+
+
+        liczba1 = (int)liczba2;
+
+        System.out.println(liczba1);
+
+        String napis = "Lubię \"białko\" :)";
+
+        System.out.println("Mój napis: " + napis);
+        System.out.print("Mój\n napis: " + napis + "\n");
+        System.out.print("Mój\t napis: " + napis);
+
+
+
+
+
+        char znak = '#';
+
+        boolean twierdzenie = false; //true
+
+
+        double liczba4 =2;
+        double liczba5 = 3;
+        double potega;
+
+        // 2 do trzeciej
+        potega = Math.pow(2, 3);
+
+        int hours = 8;
+        int days = 5;
+        int weeks = 52;
+
+        int workHoursPerYear = hours * days * weeks;
+
+
+        //  https://github.com/przemjan/czawartajgr32022
+
+
+
+    }
+}
